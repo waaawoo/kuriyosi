f3c16be361d9d3d180e833d915d3557d05fe279f
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-12 00:02:47 +0900

    ヘッダー機能完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery") //jQueryを追加
 require("bootstrap") //Bootstrapを追加
+require("../burger");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,3 +17,4 @@ require("bootstrap") //Bootstrapを追加
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
