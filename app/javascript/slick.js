0f0d69f8e38e875e183d7f09aba0fa733c7fb583
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-12 14:37:05 +0900

    スライダー導入完了

--- app/javascript/slick.js
+++ app/javascript/slick.js
@@ -1,5 +1,8 @@
-$(function(){
-  $(".slider").slick({
-
-  });
+$(function() {
+  $('.slider').slick({
+    // 矢印表示
+    nextArrow: '<i class="fas fa-arrow-right"></i>',
+    prevArrow: '<i class="fas fa-arrow-left"></i>'
+  })
 });
+
