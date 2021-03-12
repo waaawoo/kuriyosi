f4dc0f337cbfb2c0a14bbe66485c8f2efa356070
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-12 23:25:17 +0900

    不正アクセス時、トップ画面へ飛ばす

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+
+
   # ログイン時にitemindexへ飛ばす
   def after_sign_in_path_for(resource)
     if current_user
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,27 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :auth_Check
   def index
 
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def items_list
+
+  end
+  private
+
+  # 管理者以外はルートぱすへ遷移
+  def auth_Check
+    if !user_signed_in?
+      redirect_to root_path
+    end
   end
 
 end
