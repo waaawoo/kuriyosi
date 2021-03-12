963e06676c5c9eabdcfa5b752d624979e7a77062
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-12 12:02:30 +0900

    スライダー実装準備完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery") //jQueryを追加
 require("bootstrap") //Bootstrapを追加
-require("../burger");
+require("../burger")
+require("../slider")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,4 +18,3 @@ require("../burger");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
