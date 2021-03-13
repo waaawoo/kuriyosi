ba745e95f78ca4eb26092f563606d5788063aa69
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-13 20:30:35 +0900

    ベーシック認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :devise_controller?
 
 
-
+  private
   # ログイン時にitemindexへ飛ばす
   def after_sign_in_path_for(resource)
     if current_user
       items_path
+    end
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      # 環境変数が指定される
+      # vim ~/.zshrc
+      # source ~/.zshrc
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
