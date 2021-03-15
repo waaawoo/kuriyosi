b469a940fff8f82f20ce18e348b20135241d0b1b
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-15 17:50:40 +0900

    商品登録時のプレビュー機能完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("jquery") //jQueryを追加
 require('bootstrap/dist/js/bootstrap.min.js')
 require("../burger")
 require("../slick")
+require("../preview")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
