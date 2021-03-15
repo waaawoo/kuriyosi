76a5f5d15f0805ba7bef4ead079aeed231a9f19f
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-15 11:47:12 +0900

    商品登録コントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -27,6 +32,18 @@ class ItemsController < ApplicationController
     if !user_signed_in?
       redirect_to root_path
     end
+  end
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :content,
+      :price,
+      :category_id,
+      :season_id,
+      :sale_id,
+      :image
+    ).merge(user_id: current_user.id)
   end
 
 end
