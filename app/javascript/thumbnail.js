da98b858a8be3d9b7ae34f8522f8a91eb36e078a
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-16 23:05:03 +0900

    フラッシュメッセージ実装

--- app/javascript/thumbnail.js
+++ app/javascript/thumbnail.js
@@ -1,9 +1,13 @@
 $(function(){
 
+  // サムネイル切り替え
   $(".thumbnail_image").on('click', function(){
     // srcを取得して
     let dataUrl = $(this).attr("src");
     // 書き換える
     $("#main_image_top").attr('src',dataUrl);
   });
+
+  // FlashMessage
+  setTimeout("$('.flash_message').fadeOut('slow')", 1500)
 });
