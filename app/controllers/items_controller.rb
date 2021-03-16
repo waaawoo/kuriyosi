0ae24cd97702fb15263fa5b1f2a8407a3de81712
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-16 19:18:03 +0900

    複数プレビュー実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    binding.pry
     if @item.save
       redirect_to items_path
     else
--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,14 +1,37 @@
 $(function(){
-  $('#item-image').change(function(){
-      $('img').remove();
-      var file = $(this).prop('files')[0];
-      if(!file.type.match('image.*')){
-          return;
-      }
-      var fileReader = new FileReader();
-      fileReader.onloadend = function() {
-          $('.preview').html('<img src="' + fileReader.result + '"/>');
-      }
-      fileReader.readAsDataURL(file);
-  });
+        $(".file").on('change', function(){
+          let fileprop = $(this).prop('files')[0],
+              find_img = $(this).parent().find('img'),
+              filereader = new FileReader(),
+              view_box = $(this).parent('.item_image__filed');
+
+         if(find_img.length){
+            find_img.nextAll().remove();
+            find_img.remove();
+         }
+
+         let img = '<div class="img_view"><img alt="" class="img"><p><a href="#" class="img_del">画像を削除する</a></p></div>';
+
+         view_box.append(img);
+
+         filereader.onload = function() {
+           view_box.find('img').attr('src', filereader.result);
+           img_del(view_box);
+         }
+         filereader.readAsDataURL(fileprop);
+       });
+
+       function img_del(target){
+         target.find("a.img_del").on('click',function(){
+           let self = $(this),
+               parentBox = self.parent().parent().parent();
+           if(window.confirm('画像を削除します。\nよろしいですか？')){
+             setTimeout(function(){
+               parentBox.find('input[type=file]').val('');
+               parentBox.find('.img_view').remove();
+             } , 0);
+           }
+           return false;
+         });
+       }
 });
