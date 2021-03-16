9347e596f8777a7bcbd89fcd879f19175ab4bcb3
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-16 11:07:28 +0900

    スリックの自動スライド実装

--- app/javascript/slick.js
+++ app/javascript/slick.js
@@ -2,7 +2,10 @@ $(function() {
   $('.slider').slick({
     // 矢印表示
     nextArrow: '<i class="fas fa-arrow-right"></i>',
-    prevArrow: '<i class="fas fa-arrow-left"></i>'
+    prevArrow: '<i class="fas fa-arrow-left"></i>',
+
+    // 自動スライド
+    autoplay:true
   })
 });
 
