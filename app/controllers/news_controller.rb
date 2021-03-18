b157d918ac5c9f138818a721bfcc229c2625ab20
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-18 22:38:22 +0900

    新着情報登録機能実装

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -8,11 +8,28 @@ class NewsController < ApplicationController
     @news = News.new
   end
 
+  def create
+    @news = News.new(news_params)
+    if @news.save
+      redirect_to items_path, notice: "登録に成功しました"
+    else
+      render :new
+    end
+  end
+
   private
    # 管理者以外はルートぱすへ遷移
    def auth_Check
     if !user_signed_in?
       redirect_to root_path
     end
+  end
+
+  def news_params
+    params.require(:news).permit(
+      :title,
+      :content,
+      :image
+    ).merge(user_id: current_user.id)
   end
 end
