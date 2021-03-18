061889d5dd2701d67081b471aed8f6b01104e0c8
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-18 22:28:09 +0900

    新着情報登録画面実装

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,18 @@
 class NewsController < ApplicationController
+  before_action :auth_Check, only: [:new, :create, :upadate, :destroy]
   def index
 
+  end
+
+  def new
+    @news = News.new
+  end
+
+  private
+   # 管理者以外はルートぱすへ遷移
+   def auth_Check
+    if !user_signed_in?
+      redirect_to root_path
+    end
   end
 end
