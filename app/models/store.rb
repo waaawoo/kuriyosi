77048e9c03cda12b56ef29d91dfade731838fc14
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-19 19:30:45 +0900

    店舗用モデルバリデーション、アソシエーション記載

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,21 @@
 class Store < ApplicationRecord
+
+  # バリデーション
+  with_options presence: true do
+    validates :name
+    validates :phone
+    validates :address
+    validates :open_time
+    validates :close_time
+    with_options numericality: { other_than: 1 } do
+      validates :holiday_id
+    end
+  end
+
+  # モジュールの読み込み
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  # activehash
+  belongs_to_active_hash :holiday
+  # activestorage
+  has_many_attached :images
 end
