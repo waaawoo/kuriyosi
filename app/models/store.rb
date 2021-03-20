042996324a2c1aeac3134798bd1d22c759bfc7d7
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-20 14:01:42 +0900

    モデル、コントローラーを修正

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,21 +1,21 @@
 class Store < ApplicationRecord
-
-  # バリデーション
-  with_options presence: true do
-    validates :name
-    validates :phone
-    validates :address
-    validates :open_time
-    validates :close_time
-    with_options numericality: { other_than: 1 } do
-      validates :holiday_id
+    # バリデーション
+    with_options presence: true do
+      validates :name
+      validates :phone
+      validates :address
+      validates :open_time
+      validates :close_time
+      validates :images
+      with_options numericality: { other_than: 1 } do
+        validates :holiday_id
+      end
     end
-  end
 
-  # モジュールの読み込み
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  # activehash
-  belongs_to_active_hash :holiday
-  # activestorage
-  has_many_attached :images
+    # モジュールの読み込み
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    # activehash
+    belongs_to_active_hash :holiday
+    # activestorage
+    has_many_attached :images
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_153644) do
+ActiveRecord::Schema.define(version: 2021_03_20_041320) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(version: 2021_03_19_153644) do
     t.string "name", null: false
     t.string "phone", null: false
     t.string "address", null: false
-    t.string "open_time", null: false
-    t.string "close_time", null: false
+    t.time "open_time", null: false
+    t.time "close_time", null: false
     t.integer "holiday_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
