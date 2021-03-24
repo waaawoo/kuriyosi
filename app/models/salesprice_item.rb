5407bcbca7a5b3f36c6675738fc5b3dae485510b
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-24 18:22:09 +0900

    バリデーション記載

--- app/models/salesprice_item.rb
+++ app/models/salesprice_item.rb
@@ -1,4 +1,12 @@
 class SalespriceItem
   include ActiveModel::Model
   attr_accessor :day, :price, :item_id, :num, :sales_price_id
+
+  with_options presence: true do
+    validates :day
+    validates :price
+    validates :item_id
+    validates :num
+    validates :sales_price_id
+  end
 end
