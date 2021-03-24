746cda9a528e18644a53da109a0fdcd3e4109a94
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-24 18:30:25 +0900

    データ保存用の記述を追加

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -6,10 +6,16 @@ class SalesController < ApplicationController
   end
 
   def new
-
+    @sales = SalespriceItem.new
   end
 
   def create
+    @sales = SalespriceItem.new(sales_params)
+  end
 
+  private
+
+  def sales_params
+    params.require(:sales).permit(:day, :price, :item_id, :num, :sales_price_id)
   end
 end
--- app/models/salesprice_item.rb
+++ app/models/salesprice_item.rb
@@ -9,4 +9,9 @@ class SalespriceItem
     validates :num
     validates :sales_price_id
   end
+
+  def save
+    sales_price = SalesPrice.create(day: day, price: price)
+    SalesItem.create(item_id: item_id, num: num, sales_price_id: sales_price.id)
+  end
 end
