88b4cfcf7912b9882c4bce45e479f6ca8476019b
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-26 19:18:57 +0900

    取得情報をソートして10件までに制限

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,9 +1,10 @@
 class SalesController < ApplicationController
   before_action :auth_Check
-  before_action :search_slase, only: [:index, :search]
 
   def index
     @sales = SalesPrice.all
+    @p = SalesPrice.ransack(params[:q])  # 検索オブジェクトを生成
+    @results = @p.result(distinct: true).order(day: :desc).limit(10) # 検索条件にマッチした商品の情報を取得
   end
 
   def new
@@ -22,23 +23,9 @@ class SalesController < ApplicationController
     end
   end
 
-  def search
-    # binding.pry
-    @results = @p.result(distinct: true)# 検索条件にマッチした商品の情報を取得
-  end
-
   private
 
   def sales_params
     params.require(:sales_price).permit(:day, :price)
   end
-
-  def search_slase
-    @p = SalesPrice.ransack(params[:q])  # 検索オブジェクトを生成
-  end
-
-  # def set_day_column
-  #   @day_info = SalesPrice.select("day").distinct
-  # end
-
 end
